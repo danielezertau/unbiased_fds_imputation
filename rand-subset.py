2b05a28cf3918d17deaa99e5ed69aea1a96e4401
danielezertau <danielezer@mail.tau.ac.il>
2025-04-12 16:33:20 +0300

    Add code for creating random subsets of the input file

---
+++ rand-subset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Parameters
+subset_size = 5000  # or any number of rows you want to sample
+input_file = 'data/adult.csv'
+output_file = f'data/adult-rand-{subset_size}.csv'
+
+
+if __name__ == '__main__':
+    # Read the CSV file
+    df = pd.read_csv(input_file)
+
+    # Sample a random subset of rows
+    subset = df.sample(n=min(subset_size, len(df)), random_state=42)
+
+    # Write the subset to a new CSV file, including the header
+    subset.to_csv(output_file, index=False)
